4af8ec93461757940b4ac1297e7f0928930f5a53
Desmond Bowe <desmond@crevalle.io>
2020-10-24 22:08:24 -0700

    Update mix for publishing

--- mix.exs
+++ mix.exs
@@ -6,7 +6,9 @@ defmodule FortuneCookie.MixProject do
       app: :fortune_cookie,
       version: "0.1.0",
       elixir: "~> 1.10",
+      description: description(),
       author: "Desmond Bowe",
+      package: package(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -22,5 +24,19 @@ defmodule FortuneCookie.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     []
+  end
+
+  defp description do
+    "A magical way to tell your future, make big decisions, or just show an MOTD."
+  end
+
+  defp package do
+   [
+      maintainers: ["Desmond Bowe"],
+      licenses: ["MIT"],
+      links: %{
+        GitHub: "https://github.com/desmondmonster/fortune_cookie"
+      }
+    ]
   end
 end
